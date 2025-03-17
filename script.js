dbfb73e36bc41a350399c54d3d855d8015d86a0f
omprakash49 <omprakashd499@gmail.com>
2025-03-17 17:49:51 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function task1(){
+    setTimeout(() => {
+        console.log("hello world! (task 1) ");
+    }, 3000);
+}
+
+function task2(){
+    setTimeout(() => {
+        console.log("task 2 completed");
+    }, 1000)
+}
