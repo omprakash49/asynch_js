73ea58edd5c9216fdaed854a321bd1376b23d51b
omprakash49 <omprakashd499@gmail.com>
2025-03-31 15:17:45 +0530

    joke generated completed

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ let jokePara2 = document.getElementById('jokePara2');
 let but2 = document.getElementById('but2');
 
 
+let aryJoke = [];
+
+
  
 button.addEventListener("click" , randoJoke);
 
@@ -33,12 +36,49 @@ but2.addEventListener("click" , saveJoke);
 
 
 function saveJoke(){
+    jokeobj = {
+        "setup" : jokePara.innerHTML,
+        "punchline" : jokePara2.innerHTML
+    }
 
+    aryJoke.push(jokeobj);
+
+    // store in local storage
+    localStorage.setItem("aryJokeKey" , JSON.stringify(aryJoke));
+    console.log(aryJoke);
+
+    location.reload();
 }
 
 window.onload = function() {
-    let aryJoke = [];
-    // get elemet from local to arry
-
     
+    // get elemet from local to arry
+    if(localStorage.getItem("aryJokeKey") !== null )
+        aryJoke =  JSON.parse(localStorage.getItem("aryJokeKey"));
+    console.log(aryJoke);   
+    displayJokes();
+}
+
+function displayJokes(){
+    
+    for (let joke of aryJoke){
+        // section divider between different jokes
+        let hLine = document.createElement("hr");
+        content.appendChild(hLine);
+
+        // joke display
+        let disSet =  document.createElement("p");
+        let disPun = document.createElement("p");
+
+
+        disSet.innerHTML = joke.setup;
+        disPun.innerHTML = joke.punchline;
+
+        content.appendChild(disSet);
+        content.appendChild(disPun);
+
+       
+
+    }
+
 }
